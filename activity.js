edf304fd8a64165fa8f54c357cee87c7125b1b59
Bruce Gordon <brc.gordon@gmail.com>
2020-09-08 17:49:02 -0600

    Build out activity class in activity.js

--- activity.js
+++ activity.js
@@ -1,1 +1,19 @@
+class Activity {
+  constructor(category, description, minutes, seconds) {
+    this.id = Date.now();
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.complete = false;
+  }
+  countdown() {
 
+  }
+  markComplete() {
+
+  }
+  saveToStorage() {
+    
+  }
+}

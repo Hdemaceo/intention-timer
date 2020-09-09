d014f01cf4d91e62208f1d457628a7a7caa91d24
Demaceo Howard <hdemaceo@gmail.com>
2020-09-08 19:41:35 -0600

    Refactor main.js

--- main.js
+++ main.js
@@ -10,22 +10,21 @@ meditationButton.addEventListener("click", changeMeditationColor);
 exerciseButton.addEventListener("click", changeExerciseColor);
 
 //FUNCTIONS:
-
-function toggleStudyIcon(){
+function toggleStudyIcon() {
   document.querySelector(".study-icon").classList.remove("hidden");
   document.querySelector(".study-icon-active").classList.add("hidden");
 }
-function toggleMeditationIcon(){
+function toggleMeditationIcon() {
   document.querySelector(".meditate-icon").classList.remove("hidden");
   document.querySelector(".meditate-icon-active").classList.add("hidden");
 }
 
-function toggleExerciseIcon(){
+function toggleExerciseIcon() {
   document.querySelector(".exercise-icon").classList.remove("hidden");
   document.querySelector(".exercise-icon-active").classList.add("hidden");
 }
 
-function resetColors(){
+function resetColors() {
   studyButton.setAttribute("id", "");
   meditationButton.setAttribute("id", "");
   exerciseButton.setAttribute("id", "");
@@ -40,27 +39,29 @@ function changeStudyColor() {
     studyButton.setAttribute("id", "green");
   } else {
     studyButton.setAttribute("id", "");
-  }
+  };
   document.querySelector(".study-icon").classList.toggle("hidden");
   document.querySelector(".study-icon-active").classList.toggle("hidden");
 }
+
 function changeMeditationColor() {
   resetColors();
   if(meditationButton.id === ""){
     meditationButton.setAttribute("id", "purple");
   } else {
     meditationButton.setAttribute("id", "");
-  }
+  };
   document.querySelector(".meditate-icon").classList.toggle("hidden");
   document.querySelector(".meditate-icon-active").classList.toggle("hidden");
 }
+
 function changeExerciseColor() {
   resetColors();
   if(exerciseButton.id === ""){
     exerciseButton.setAttribute("id", "orange");
   } else {
     exerciseButton.setAttribute("id", "");
-  }
+  };
   document.querySelector(".exercise-icon").classList.toggle("hidden");
   document.querySelector(".exercise-icon-active").classList.toggle("hidden");
 }

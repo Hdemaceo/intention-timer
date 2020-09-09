b4ff99f34a6bf65cb9a2e6fcb3a0ac4729162f59
Demaceo Howard <hdemaceo@gmail.com>
2020-09-08 19:39:43 -0600

    Create and add functionality to resetColors function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var studyButton = document.querySelector(".study");
 var meditationButton = document.querySelector(".meditate");
 var exerciseButton = document.querySelector(".exercise");
-var green = document.querySelector("#green");
+
 
 //EVENT LISTENERS:
 studyButton.addEventListener("click", changeStudyColor);
@@ -10,7 +10,32 @@ meditationButton.addEventListener("click", changeMeditationColor);
 exerciseButton.addEventListener("click", changeExerciseColor);
 
 //FUNCTIONS:
+
+function toggleStudyIcon(){
+  document.querySelector(".study-icon").classList.remove("hidden");
+  document.querySelector(".study-icon-active").classList.add("hidden");
+}
+function toggleMeditationIcon(){
+  document.querySelector(".meditate-icon").classList.remove("hidden");
+  document.querySelector(".meditate-icon-active").classList.add("hidden");
+}
+
+function toggleExerciseIcon(){
+  document.querySelector(".exercise-icon").classList.remove("hidden");
+  document.querySelector(".exercise-icon-active").classList.add("hidden");
+}
+
+function resetColors(){
+  studyButton.setAttribute("id", "");
+  meditationButton.setAttribute("id", "");
+  exerciseButton.setAttribute("id", "");
+  toggleMeditationIcon();
+  toggleStudyIcon();
+  toggleExerciseIcon();
+}
+
 function changeStudyColor() {
+  resetColors();
   if(studyButton.id === ""){
     studyButton.setAttribute("id", "green");
   } else {
@@ -19,8 +44,8 @@ function changeStudyColor() {
   document.querySelector(".study-icon").classList.toggle("hidden");
   document.querySelector(".study-icon-active").classList.toggle("hidden");
 }
-
 function changeMeditationColor() {
+  resetColors();
   if(meditationButton.id === ""){
     meditationButton.setAttribute("id", "purple");
   } else {
@@ -29,8 +54,8 @@ function changeMeditationColor() {
   document.querySelector(".meditate-icon").classList.toggle("hidden");
   document.querySelector(".meditate-icon-active").classList.toggle("hidden");
 }
-
 function changeExerciseColor() {
+  resetColors();
   if(exerciseButton.id === ""){
     exerciseButton.setAttribute("id", "orange");
   } else {

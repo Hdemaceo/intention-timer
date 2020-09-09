53b19cf4e7afebc318e2869c21c305e8fda1eb24
Demaceo Howard <hdemaceo@gmail.com>
2020-09-08 18:14:44 -0600

    Create event listeners for category buttons

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 var studyButton = document.querySelector(".study");
 var meditationButton = document.querySelector(".meditate");
 var exerciseButton = document.querySelector(".exercise");
+
+//EVENT LISTENERS:
+studyButton.addEventListener("click", changeStudyColor);
+meditationButton.addEventListener("click", changeMeditationColor);
+exerciseButton.addEventListener("click", changeExerciseColor);

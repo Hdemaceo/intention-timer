3c9289cc9cc6525da4ee4af16406eec5011ff9d4
Demaceo Howard <hdemaceo@gmail.com>
2020-09-08 19:31:10 -0600

    Create and add functionality to changeStudyColor function

--- main.js
+++ main.js
@@ -2,8 +2,20 @@
 var studyButton = document.querySelector(".study");
 var meditationButton = document.querySelector(".meditate");
 var exerciseButton = document.querySelector(".exercise");
+var green = document.querySelector("#green");
 
 //EVENT LISTENERS:
 studyButton.addEventListener("click", changeStudyColor);
 meditationButton.addEventListener("click", changeMeditationColor);
 exerciseButton.addEventListener("click", changeExerciseColor);
+
+//FUNCTIONS:
+function changeStudyColor() {
+  if(studyButton.id === ""){
+    studyButton.setAttribute("id", "green");
+  } else {
+    studyButton.setAttribute("id", "");
+  }
+  document.querySelector(".study-icon").classList.toggle("hidden");
+  document.querySelector(".study-icon-active").classList.toggle("hidden");
+}
